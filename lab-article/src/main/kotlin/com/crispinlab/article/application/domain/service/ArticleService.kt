ec4ba663f021d3948c1aa032be1b5540549a937a
crispin <shoy1415@gmail.com>
2025-03-19 18:08:48 +0900

    :sparkles: Feat: article write logic

--- lab-article/src/main/kotlin/com/crispinlab/article/application/domain/service/ArticleService.kt
+++ lab-article/src/main/kotlin/com/crispinlab/article/application/domain/service/ArticleService.kt
@@ -1,14 +1,24 @@
 package com.crispinlab.article.application.domain.service
 
+import com.crispinlab.Snowflake
+import com.crispinlab.article.application.domain.extensions.toDomain
+import com.crispinlab.article.application.domain.extensions.toDto
+import com.crispinlab.article.application.domain.model.Article
 import com.crispinlab.article.application.port.input.WriteArticleUseCase
 import com.crispinlab.article.application.port.output.WriteArticlePort
 import org.springframework.stereotype.Service
 
 @Service
 internal class ArticleService(
+    private val snowflake: Snowflake,
     private val writeArticlePort: WriteArticlePort
 ) : WriteArticleUseCase {
     override fun write(request: WriteArticleUseCase.Request): WriteArticleUseCase.Response {
-        TODO("Not yet implemented")
+        val article: Article = request.toDomain(snowflake.nextId())
+        writeArticlePort.saveArticle(article.toDto())
+        return WriteArticleUseCase.Response(
+            id = article.id,
+            createdAt = article.createdAt
+        )
     }
 }
