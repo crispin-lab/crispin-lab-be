edf70688017772cba8d58bcb5f74e9024a6f385f
crispin <h.c.shin.dev09@gmail.com>
2025-03-22 12:02:22 +0900

    :recycle: Refactor: redistribute responsibilities for dto translation

--- lab-article/src/main/kotlin/com/crispinlab/article/application/port/output/WriteArticlePort.kt
+++ lab-article/src/main/kotlin/com/crispinlab/article/application/port/output/WriteArticlePort.kt
@@ -1,19 +1,7 @@
 package com.crispinlab.article.application.port.output
 
-import com.crispinlab.article.application.domain.model.VisibilityType
-import java.time.Instant
+import com.crispinlab.article.application.domain.model.Article
 
 internal interface WriteArticlePort {
-    data class Request(
-        val id: Long,
-        var title: String,
-        var content: String,
-        val author: Long,
-        val board: Long,
-        val visibility: VisibilityType,
-        val createdAt: Instant,
-        var modifiedAt: Instant
-    )
-
-    fun saveArticle(request: Request)
+    fun saveArticle(article: Article)
 }
