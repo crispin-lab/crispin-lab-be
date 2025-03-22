cef6957008e91e8873c3c45034240cc75c899b68
crispin <h.c.shin.dev09@gmail.com>
2025-03-22 19:53:38 +0900

    :sparkles: Feat: add article to dto

--- lab-article/src/main/kotlin/com/crispinlab/article/application/domain/extensions/ArticleDomainExteions.kt
+++ lab-article/src/main/kotlin/com/crispinlab/article/application/domain/extensions/ArticleDomainExteions.kt
@@ -1,6 +1,7 @@
 package com.crispinlab.article.application.domain.extensions
 
 import com.crispinlab.article.application.domain.model.Article
+import com.crispinlab.article.application.port.input.ReadArticleUseCase
 import com.crispinlab.article.application.port.input.WriteArticleUseCase
 
 internal fun WriteArticleUseCase.WriteRequest.toDomain(id: Long): Article =
@@ -11,4 +12,14 @@ internal fun WriteArticleUseCase.WriteRequest.toDomain(id: Long): Article =
         author = this.author,
         board = this.board,
         visibility = this.visibility
+    )
+
+internal fun Article.toDto(): ReadArticleUseCase.GetDetailResponse =
+    ReadArticleUseCase.GetDetailResponse(
+        title = this.title,
+        content = this.content,
+        author = this.author,
+        board = this.board,
+        createdAt = this.createdAt,
+        modifiedAt = this.modifiedAt
     )
