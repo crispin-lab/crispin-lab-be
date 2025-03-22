7a2f3123ab6f3a3f419107de44e525cd98b09014
crispin <h.c.shin.dev09@gmail.com>
2025-03-22 21:41:00 +0900

    :heavy_plus_sign: Add: kotlin serialization dependency

--- lab-article/build.gradle.kts
+++ lab-article/build.gradle.kts
@@ -3,3 +3,6 @@ plugins {
     alias(libs.plugins.kotlin.spring.web)
     alias(libs.plugins.kotlin.snowflake)
 }
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+}
