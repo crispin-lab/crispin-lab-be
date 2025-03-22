8e8aac858708f0a92766e6675a00248719ead56a
crispin <h.c.shin.dev09@gmail.com>
2025-03-22 21:41:00 +0900

    :heavy_plus_sign: Add: kotlin serialization dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.spring.boot) apply false
     alias(libs.plugins.kotlin.plugin.spring) apply false
     alias(libs.plugins.spring.dependency.management) apply false
+    alias(libs.plugins.kotlin.plugin.serialization) apply false
 }
