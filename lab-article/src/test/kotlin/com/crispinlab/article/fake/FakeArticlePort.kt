e0ca208cf0a411f55d8a83c2255061111c4a3b1b
crispin <h.c.shin.dev09@gmail.com>
2025-03-22 11:55:02 +0900

    :white_check_mark: Test: add read article detail test

--- lab-article/src/test/kotlin/com/crispinlab/article/fake/FakeArticlePort.kt
+++ lab-article/src/test/kotlin/com/crispinlab/article/fake/FakeArticlePort.kt
@@ -1,11 +1,27 @@
 package com.crispinlab.article.fake
 
+import com.crispinlab.article.application.domain.model.Article
+import com.crispinlab.article.application.port.output.ReadArticlePort
 import com.crispinlab.article.application.port.output.WriteArticlePort
 
-internal class FakeArticlePort : WriteArticlePort {
-    private val storage: MutableMap<Long, WriteArticlePort.Request> = mutableMapOf()
+internal class FakeArticlePort :
+    WriteArticlePort,
+    ReadArticlePort {
+    private val storage: MutableMap<Long, Article> = mutableMapOf()
 
     override fun saveArticle(request: WriteArticlePort.Request) {
-        storage[request.id] = request
+        storage[request.id] =
+            Article(
+                request.id,
+                request.title,
+                request.content,
+                request.author,
+                request.board,
+                request.visibility,
+                request.createdAt,
+                request.modifiedAt
+            )
     }
+
+    override fun getArticleBy(id: Long): Article = storage[id]!!
 }
