a1233d2c832582984b0bc3df7ca948a7905ee0c4
crispin <h.c.shin.dev09@gmail.com>
2025-03-22 11:55:02 +0900

    :white_check_mark: Test: add read article detail test

--- lab-article/src/test/kotlin/com/crispinlab/article/application/domain/service/ArticleServiceTest.kt
+++ lab-article/src/test/kotlin/com/crispinlab/article/application/domain/service/ArticleServiceTest.kt
@@ -1,10 +1,13 @@
 package com.crispinlab.article.application.domain.service
 
 import com.crispinlab.Snowflake
+import com.crispinlab.article.application.port.input.ReadArticleUseCase
 import com.crispinlab.article.application.port.input.WriteArticleUseCase
+import com.crispinlab.article.application.port.output.ReadArticlePort
 import com.crispinlab.article.application.port.output.WriteArticlePort
 import com.crispinlab.article.fake.FakeArticlePort
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 
 class ArticleServiceTest :
@@ -12,13 +15,17 @@ class ArticleServiceTest :
         val snowflake: Snowflake = Snowflake.create(777)
         lateinit var articleService: ArticleService
         lateinit var writeArticlePort: WriteArticlePort
+        lateinit var readArticlePort: ReadArticlePort
 
         beforeTest {
-            writeArticlePort = FakeArticlePort()
+            val fakeArticlePort = FakeArticlePort()
+            writeArticlePort = fakeArticlePort
+            readArticlePort = fakeArticlePort
             articleService =
                 ArticleService(
                     writeArticlePort = writeArticlePort,
-                    snowflake = snowflake
+                    snowflake = snowflake,
+                    readArticlePort = readArticlePort
                 )
         }
 
@@ -39,6 +46,33 @@ class ArticleServiceTest :
 
                     // then
                     actual shouldNotBe null
+                }
+            }
+        }
+
+        describe("ReadDetail") {
+            context("Success") {
+                it("게시글 상세 조회 성공 테스트") {
+                    // given
+                    val authorId: Long = snowflake.nextId()
+                    val saveRequest =
+                        WriteArticleUseCase.Request(
+                            title = "코틀린 테스트 작성 방법",
+                            content = "테스트를 작성한다.",
+                            author = authorId,
+                            board = snowflake.nextId()
+                        )
+                    val request =
+                        ReadArticleUseCase.GetDetailRequest(
+                            articleService.write(saveRequest).id
+                        )
+
+                    // when
+                    val actual: ReadArticleUseCase.GetDetailResponse =
+                        articleService.readDetail(request)
+
+                    // then
+                    actual.title shouldBe "코틀린 테스트 작성 방법"
                 }
             }
         }
