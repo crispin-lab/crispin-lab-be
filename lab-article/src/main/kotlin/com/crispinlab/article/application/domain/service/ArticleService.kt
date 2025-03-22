04daf31bf7c4ae2c216573b87184b3179c5e0326
crispin <h.c.shin.dev09@gmail.com>
2025-03-22 19:54:30 +0900

    :sparkles: Feat: add read all articles

--- lab-article/src/main/kotlin/com/crispinlab/article/application/domain/service/ArticleService.kt
+++ lab-article/src/main/kotlin/com/crispinlab/article/application/domain/service/ArticleService.kt
@@ -2,12 +2,14 @@ package com.crispinlab.article.application.domain.service
 
 import com.crispinlab.Snowflake
 import com.crispinlab.article.application.domain.extensions.toDomain
+import com.crispinlab.article.application.domain.extensions.toDto
 import com.crispinlab.article.application.domain.model.Article
 import com.crispinlab.article.application.port.input.ReadArticleUseCase
 import com.crispinlab.article.application.port.input.WriteArticleUseCase
 import com.crispinlab.article.application.port.output.ReadArticlePort
 import com.crispinlab.article.application.port.output.WriteArticlePort
 import com.crispinlab.article.common.exception.ArticleNotFoundException
+import com.crispinlab.article.common.util.PageLimitCalculator
 import org.springframework.stereotype.Service
 
 @Service
@@ -66,5 +68,19 @@ internal class ArticleService(
         readArticlePort.getArticleBy(request.articleId)?.let {
             writeArticlePort.deleteArticle(request.articleId)
         } ?: throw ArticleNotFoundException(message = "존재하지 않는 게시글 ID 입니다. ${request.articleId}")
+    }
+
+    override fun readAll(
+        request: ReadArticleUseCase.GetReadAllRequest
+    ): ReadArticleUseCase.GetReadAllResponse {
+        val pageLimit: Long =
+            PageLimitCalculator.calculatePageLimit(page = request.page, pageSize = request.pageSize)
+        val count: Long = readArticlePort.count(request.boardId, pageLimit)
+        val articles: List<Article> =
+            readArticlePort.getArticlesBy(request.boardId, request.page, request.pageSize)
+        return ReadArticleUseCase.GetReadAllResponse(
+            articles = articles.map { it.toDto() },
+            articleCount = count
+        )
     }
 }
