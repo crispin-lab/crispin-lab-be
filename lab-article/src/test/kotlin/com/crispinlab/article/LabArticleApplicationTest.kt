e91c490cde8bc68b37b6fdde70588b4fa12fbf05
crispin <h.c.shin.dev09@gmail.com>
2025-03-23 23:56:56 +0900

    :white_check_mark: Test: add spring boot application test

---
+++ lab-article/src/test/kotlin/com/crispinlab/article/LabArticleApplicationTest.kt
@@ -0,0 +1,11 @@
+package com.crispinlab.article
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LabArticleApplicationTest {
+    @Test
+    fun contextLoads() {
+    }
+}
