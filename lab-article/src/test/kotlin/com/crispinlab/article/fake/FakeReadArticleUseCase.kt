efb9a4858dfa3211d78f63d9e6a7c8bcda5a5606
crispin <h.c.shin.dev09@gmail.com>
2025-03-24 23:39:37 +0900

    :sparkles: Feat: add fake read article usecase

---
+++ lab-article/src/test/kotlin/com/crispinlab/article/fake/FakeReadArticleUseCase.kt
@@ -0,0 +1,27 @@
+package com.crispinlab.article.fake
+
+import com.crispinlab.article.application.port.input.ReadArticleUseCase
+import com.crispinlab.article.fixture.snowflake
+import java.time.Instant
+
+internal class FakeReadArticleUseCase : ReadArticleUseCase {
+    override fun readDetail(
+        request: ReadArticleUseCase.GetDetailRequest
+    ): ReadArticleUseCase.GetDetailResponse =
+        ReadArticleUseCase.GetDetailResponse(
+            29929630239592448,
+            "코틀린 컨트롤러 테스트 작성 방법",
+            "테스트를 작성한다.",
+            snowflake.nextId(),
+            snowflake.nextId(),
+            "PUBLIC",
+            Instant.now(),
+            Instant.now()
+        )
+
+    override fun readAll(
+        request: ReadArticleUseCase.GetReadAllRequest
+    ): ReadArticleUseCase.GetReadAllResponse {
+        TODO("Not yet implemented")
+    }
+}
