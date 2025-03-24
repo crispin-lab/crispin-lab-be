f378f97affd81943c204b655e2d5522ea7a865f9
crispin <h.c.shin.dev09@gmail.com>
2025-03-24 23:37:51 +0900

    :sparkles: Feat: add id, visibility field

--- lab-article/src/main/kotlin/com/crispinlab/article/application/port/input/ReadArticleUseCase.kt
+++ lab-article/src/main/kotlin/com/crispinlab/article/application/port/input/ReadArticleUseCase.kt
@@ -8,12 +8,14 @@ internal interface ReadArticleUseCase {
     )
 
     data class GetDetailResponse(
+        val id: Long,
         val title: String,
         val content: String,
         val author: Long,
         val board: Long,
-        val createdAt: Instant = Instant.now(),
-        var modifiedAt: Instant = Instant.now()
+        val visibility: String,
+        val createdAt: Instant,
+        var modifiedAt: Instant
     )
 
     data class GetReadAllRequest(
