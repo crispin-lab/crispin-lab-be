23d145325d286808e62c3ba0be137f231244126f
crispin <h.c.shin.dev09@gmail.com>
2025-03-24 23:38:36 +0900

    :sparkles: Feat: add read article web controller v1

--- lab-article/src/main/kotlin/com/crispinlab/article/adapter/input/web/ReadArticleWebControllerV1.kt
+++ lab-article/src/main/kotlin/com/crispinlab/article/adapter/input/web/ReadArticleWebControllerV1.kt
@@ -1,5 +1,7 @@
 package com.crispinlab.article.adapter.input.web
 
+import com.crispinlab.article.adapter.input.web.dto.response.ArticleResponse
+import com.crispinlab.article.adapter.input.web.dto.response.ReadArticleResponse
 import com.crispinlab.article.application.port.input.ReadArticleUseCase
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -17,11 +19,31 @@ internal class ReadArticleWebControllerV1(
     )
     fun getArticle(
         @PathVariable articleId: Long
-    ) {
-        readArticleUseCase.readDetail(
-            ReadArticleUseCase.GetDetailRequest(
-                articleId = articleId
+    ): ArticleResponse<ReadArticleResponse> {
+        val response: ReadArticleUseCase.GetDetailResponse =
+            readArticleUseCase.readDetail(
+                ReadArticleUseCase.GetDetailRequest(
+                    articleId = articleId
+                )
             )
+        return ArticleResponse.success(
+            result =
+                ReadArticleResponse(
+                    id = response.id,
+                    title = response.title,
+                    content = response.content,
+                    author = response.author,
+                    board = response.board,
+                    visibilityType =
+                        when (response.visibility) {
+                            "PUBLIC" -> ReadArticleResponse.VisibilityType.PUBLIC
+                            "PRIVATE" -> ReadArticleResponse.VisibilityType.PRIVATE
+                            "RESTRICTED" -> ReadArticleResponse.VisibilityType.RESTRICTED
+                            else -> ReadArticleResponse.VisibilityType.PUBLIC
+                        },
+                    createdAt = response.createdAt,
+                    modifiedAt = response.modifiedAt
+                )
         )
     }
 }
