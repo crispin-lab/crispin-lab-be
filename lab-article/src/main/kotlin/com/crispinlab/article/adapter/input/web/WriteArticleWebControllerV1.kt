9dd26cdab02ae68ad09867e4cab1d6117567a68e
crispin <shoy1415@gmail.com>
2025-03-27 20:26:23 +0900

    :sparkles: Feat: add update article controller

--- lab-article/src/main/kotlin/com/crispinlab/article/adapter/input/web/WriteArticleWebControllerV1.kt
+++ lab-article/src/main/kotlin/com/crispinlab/article/adapter/input/web/WriteArticleWebControllerV1.kt
@@ -1,10 +1,12 @@
 package com.crispinlab.article.adapter.input.web
 
+import com.crispinlab.article.adapter.input.web.dto.request.UpdateArticleRequest
 import com.crispinlab.article.adapter.input.web.dto.request.WriteArticleRequest
 import com.crispinlab.article.adapter.input.web.dto.response.ArticleResponse
 import com.crispinlab.article.adapter.input.web.dto.response.WriteArticleResponse
 import com.crispinlab.article.application.port.input.WriteArticleUseCase
 import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -37,6 +39,33 @@ internal class WriteArticleWebControllerV1(
                 WriteArticleResponse(
                     id = response.id.toString(),
                     dateTime = response.createdAt
+                )
+        )
+    }
+
+    @PatchMapping(
+        path = ["/article"],
+        produces = ["application/json", "application/vnd.crispin-lab.com-v1+json"]
+    )
+    fun updateArticle(
+        @RequestBody @Valid request: UpdateArticleRequest
+    ): ArticleResponse<WriteArticleResponse> {
+        val response: WriteArticleUseCase.UpdateResponse =
+            writeArticleUseCase.update(
+                WriteArticleUseCase.UpdateRequest(
+                    articleId = request.articleId,
+                    title = request.title,
+                    content = request.content,
+                    board = request.board,
+                    visibility = request.visibility?.name,
+                    modifiedAt = request.modifiedAt
+                )
+            )
+        return ArticleResponse.success(
+            result =
+                WriteArticleResponse(
+                    id = response.articleId.toString(),
+                    dateTime = response.modifiedAt
                 )
         )
     }
