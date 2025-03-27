19a7cb8c099601c066b73f31bc05ec8c9cebf8ce
crispin <shoy1415@gmail.com>
2025-03-27 20:25:10 +0900

    :sparkles: Feat: add `@Serializable` annotation

--- lab-article/src/main/kotlin/com/crispinlab/article/adapter/input/web/dto/response/ExceptionResponse.kt
+++ lab-article/src/main/kotlin/com/crispinlab/article/adapter/input/web/dto/response/ExceptionResponse.kt
@@ -1,7 +1,9 @@
 package com.crispinlab.article.adapter.input.web.dto.response
 
 import com.fasterxml.jackson.annotation.JsonInclude
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class ExceptionResponse<T>(
     val message: String,
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
--- lab-article/src/main/kotlin/com/crispinlab/article/adapter/input/web/dto/response/WriteArticleResponse.kt
+++ lab-article/src/main/kotlin/com/crispinlab/article/adapter/input/web/dto/response/WriteArticleResponse.kt
@@ -1,8 +1,12 @@
 package com.crispinlab.article.adapter.input.web.dto.response
 
 import java.time.Instant
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class WriteArticleResponse(
     val id: String,
+    @Contextual
     val dateTime: Instant
 )
