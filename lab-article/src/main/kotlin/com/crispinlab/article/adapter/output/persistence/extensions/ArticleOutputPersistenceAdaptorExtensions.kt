c8428f9209ff47ffe0189ccf255bc90f9f5f08e4
crispin <shoy1415@gmail.com>
2025-03-31 14:09:49 +0900

    :sparkles: Feat: add article save repository

---
+++ lab-article/src/main/kotlin/com/crispinlab/article/adapter/output/persistence/extensions/ArticleOutputPersistenceAdaptorExtensions.kt
@@ -0,0 +1,22 @@
+package com.crispinlab.article.adapter.output.persistence.extensions
+
+import com.crispinlab.article.adapter.output.persistence.entity.ArticleJpaEntity
+import com.crispinlab.article.application.domain.model.Article
+import com.crispinlab.article.application.domain.model.VisibilityType
+
+internal fun Article.toEntity(): ArticleJpaEntity =
+    ArticleJpaEntity(
+        id = this.id,
+        title = this.title,
+        content = this.content,
+        authorId = this.author,
+        boardId = this.board,
+        visibilityType =
+            when (this.visibility) {
+                VisibilityType.PUBLIC -> ArticleJpaEntity.VisibilityType.PUBLIC
+                VisibilityType.PRIVATE -> ArticleJpaEntity.VisibilityType.PRIVATE
+                VisibilityType.RESTRICTED -> ArticleJpaEntity.VisibilityType.RESTRICTED
+            },
+        createdAt = this.createdAt,
+        modifedAt = this.modifiedAt
+    )
