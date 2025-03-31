1ef5a6d04b7e4af25c08836687c8e7afb6fb0c4b
crispin <shoy1415@gmail.com>
2025-03-31 15:46:33 +0900

    :white_check_mark: Test: add integration test class

---
+++ lab-article/src/test/kotlin/com/crispinlab/article/integration/IntegrationTest.kt
@@ -0,0 +1,27 @@
+package com.crispinlab.article.integration
+
+import io.restassured.RestAssured
+import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.server.LocalServerPort
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class IntegrationTest(
+    @Autowired
+    private val databaseCleanup: DatabaseCleanup
+) {
+    @LocalServerPort
+    private var port: Int = 0
+
+    @BeforeEach
+    fun setup() {
+        RestAssured.port
+            .takeIf { it == RestAssured.UNDEFINED_PORT }
+            ?.let {
+                RestAssured.port = port
+                databaseCleanup.afterPropertiesSet()
+            }
+        databaseCleanup.execute()
+    }
+}
