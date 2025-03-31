29d103c276153b5ab39b10c2ed1d7a61e77a6d3d
crispin <shoy1415@gmail.com>
2025-03-31 15:44:12 +0900

    :heavy_plus_sign: Add: rest assured library

---
+++ build-logic/convention/src/main/kotlin/RestAssuredLibraryConventionPlugin.kt
@@ -0,0 +1,14 @@
+import com.crispinlab.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+class RestAssuredLibraryConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            dependencies {
+                add("testImplementation", libs.findLibrary("rest.assured").get())
+            }
+        }
+    }
+}
