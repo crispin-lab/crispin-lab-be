eea8bb2c611e7812d84aaca167acb9b0d064674e
crispin <shoy1415@gmail.com>
2025-04-01 18:05:45 +0900

    :sparkles: Feat: add soft delete column

--- lab-article/src/main/resources/sql/schema.sql
+++ lab-article/src/main/resources/sql/schema.sql
@@ -2,14 +2,15 @@ drop table if exists articles cascade;
 
 create table articles
 (
-  is_delete       boolean                                 not null,
-  author_id       bigint                                  not null,
-  board_id        bigint                                  not null,
-  created_at      timestamp(6) with time zone             not null,
-  id              bigint                                  not null,
-  modified_at     timestamp(6) with time zone,
-  content         varchar(255)                            not null,
-  title           varchar(255)                            not null,
-  visibility_type enum('PRIVATE', 'PUBLIC', 'RESTRICTED') not null,
+  id          bigint                                  not null,
+  content     varchar(255)                            not null,
+  title       varchar(255)                            not null,
+  author_id   bigint                                  not null,
+  board_id    bigint                                  not null,
+  visibility  enum('PRIVATE', 'PUBLIC', 'RESTRICTED') not null,
+  created_at  timestamp(6) with time zone             not null,
+  modified_at timestamp(6) with time zone,
+  deleted_at  timestamp(6) with time zone,
+  is_deleted  boolean                                 not null,
   primary key (id)
 );
