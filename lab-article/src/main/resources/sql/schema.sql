11e5d682f19bf2a6db653ec8ba818e030f615df7
crispin <h.c.shin.dev09@gmail.com>
2025-04-01 23:39:29 +0900

    :bug: Fix: change column type of content
      - varchar(255) -> text

--- lab-article/src/main/resources/sql/schema.sql
+++ lab-article/src/main/resources/sql/schema.sql
@@ -2,15 +2,15 @@ drop table if exists articles cascade;
 
 create table articles
 (
-  id          bigint                                  not null,
-  content     varchar(255)                            not null,
-  title       varchar(255)                            not null,
-  author_id   bigint                                  not null,
-  board_id    bigint                                  not null,
+  id          bigint                      not null,
+  title       varchar(255)                not null,
+  content     text                        not null,
+  author_id   bigint                      not null,
+  board_id    bigint                      not null,
   visibility  enum('PRIVATE', 'PUBLIC', 'RESTRICTED') not null,
-  created_at  timestamp(6) with time zone             not null,
+  created_at  timestamp(6) with time zone not null,
   modified_at timestamp(6) with time zone,
   deleted_at  timestamp(6) with time zone,
-  is_deleted  boolean                                 not null,
+  is_deleted  boolean                     not null,
   primary key (id)
 );
