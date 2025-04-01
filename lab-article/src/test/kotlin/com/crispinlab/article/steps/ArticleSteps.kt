030ce7875295e83390013e463d3b201affeb5fd3
crispin <shoy1415@gmail.com>
2025-04-01 18:06:32 +0900

    :sparkles: Feat: add article integration test steps

---
+++ lab-article/src/test/kotlin/com/crispinlab/article/steps/ArticleSteps.kt
@@ -0,0 +1,37 @@
+package com.crispinlab.article.steps
+
+import com.crispinlab.article.adapter.input.web.dto.request.WriteArticleRequest
+import io.restassured.module.kotlin.extensions.Extract
+import io.restassured.module.kotlin.extensions.Given
+import io.restassured.module.kotlin.extensions.Then
+import io.restassured.module.kotlin.extensions.When
+import io.restassured.response.Response
+import org.springframework.http.MediaType
+
+object ArticleSteps {
+    fun articleSave(request: WriteArticleRequest): Response =
+        Given {
+            log().all().contentType(MediaType.APPLICATION_JSON_VALUE)
+            accept("application/vnd.crispin-lab.com-v1+json")
+            body(request)
+        } When {
+            post("/api/article")
+        } Then {
+            statusCode(200)
+        } Extract {
+            response()
+        }
+
+    fun getArticleBy(id: Long): Response =
+        Given {
+            log().all().contentType(MediaType.APPLICATION_JSON_VALUE)
+            accept("application/vnd.crispin-lab.com-v1+json")
+        } When {
+            get("/api/articles/$id")
+        } Then {
+            statusCode(200)
+        } Extract {
+            response().print()
+            response()
+        }
+}
