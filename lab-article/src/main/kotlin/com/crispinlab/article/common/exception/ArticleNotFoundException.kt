c468f711931bbb7ba47f0db9b9b47f2f19936445
crispin <shoy1415@gmail.com>
2025-04-01 20:01:00 +0900

    :recycle: Refactor: change message in article not found exception

--- lab-article/src/main/kotlin/com/crispinlab/article/common/exception/ArticleNotFoundException.kt
+++ lab-article/src/main/kotlin/com/crispinlab/article/common/exception/ArticleNotFoundException.kt
@@ -1,8 +1,8 @@
 package com.crispinlab.article.common.exception
 
 internal class ArticleNotFoundException(
-    override val code: String = "404",
-    override val message: String = "존재 하지 않는 게시글 입니다."
+    override val code: String = "ARTICLE_NOT_FOUND",
+    override val message: String = "존재하지 않는 게시글 입니다."
 ) : ArticleException(
         code,
         message
