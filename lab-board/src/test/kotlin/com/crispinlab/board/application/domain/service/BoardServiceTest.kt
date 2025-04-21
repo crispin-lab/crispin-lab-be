07b478f4cd4a24eaafbe4f0cc755481fbb3e0b1e
crispin <shoy1415@gmail.com>
2025-04-21 19:55:03 +0900

    :test_tube: Test: add board update test

--- lab-board/src/test/kotlin/com/crispinlab/board/application/domain/service/BoardServiceTest.kt
+++ lab-board/src/test/kotlin/com/crispinlab/board/application/domain/service/BoardServiceTest.kt
@@ -3,9 +3,11 @@ package com.crispinlab.board.application.domain.service
 import com.crispinlab.Snowflake
 import com.crispinlab.board.application.port.input.ManageBoardUseCase
 import com.crispinlab.board.application.port.output.ManageBoardPort
+import com.crispinlab.board.application.port.output.ReadBoardPort
 import com.crispinlab.board.fake.FakeBoardPort
 import com.crispinlab.board.fixture.snowflake
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 
 class BoardServiceTest :
@@ -13,13 +15,17 @@ class BoardServiceTest :
         val snowflake: Snowflake = snowflake
         lateinit var boardService: BoardService
         lateinit var manageBoardPort: ManageBoardPort
+        lateinit var readBoardPort: ReadBoardPort
 
         beforeTest {
-            manageBoardPort = FakeBoardPort()
+            val fakeBoardPort = FakeBoardPort()
+            manageBoardPort = fakeBoardPort
+            readBoardPort = fakeBoardPort
             boardService =
                 BoardService(
                     snowflake = snowflake,
-                    manageBoardPort = manageBoardPort
+                    manageBoardPort = manageBoardPort,
+                    readBoardPort = readBoardPort
                 )
         }
 
@@ -38,6 +44,37 @@ class BoardServiceTest :
 
                     // then
                     actual shouldNotBe null
+                }
+            }
+        }
+
+        /*
+        todo    :: 게시판 조회 기능 구현 후 수정
+         author :: heechoel shin
+         date   :: 2025-04-21T19:54:48KST
+         */
+        describe("Update") {
+            context("Success") {
+                it("게시판 수정 성공 테스트") {
+                    // given
+                    val createRequest =
+                        ManageBoardUseCase.CreateRequest(
+                            name = "테스트 게시판",
+                            description = "테스트용 게시판 입니다."
+                        )
+                    val boardId: Long = boardService.create(createRequest).id
+                    val request =
+                        ManageBoardUseCase.UpdateRequest(
+                            id = boardId,
+                            name = "게시판 이름 수정",
+                            description = "수정된 게시판"
+                        )
+
+                    // when
+                    boardService.update(request)
+
+                    // then
+                    1 shouldBe 1
                 }
             }
         }
--- lab-board/src/test/kotlin/com/crispinlab/board/fake/FakeBoardPort.kt
+++ lab-board/src/test/kotlin/com/crispinlab/board/fake/FakeBoardPort.kt
@@ -2,11 +2,20 @@ package com.crispinlab.board.fake
 
 import com.crispinlab.board.application.domain.model.Board
 import com.crispinlab.board.application.port.output.ManageBoardPort
+import com.crispinlab.board.application.port.output.ReadBoardPort
 
-internal class FakeBoardPort : ManageBoardPort {
+internal class FakeBoardPort :
+    ManageBoardPort,
+    ReadBoardPort {
     private val storage: MutableMap<Long, Board> = mutableMapOf()
 
     override fun saveBoard(board: Board) {
         storage[board.id] = board
     }
+
+    override fun updateBoard(board: Board) {
+        storage[board.id] = board
+    }
+
+    override fun getBoardBy(id: Long): Board? = storage[id]
 }
