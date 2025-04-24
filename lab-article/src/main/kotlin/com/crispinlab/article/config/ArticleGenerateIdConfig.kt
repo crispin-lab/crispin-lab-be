3fc5f9f6de1d311129ed770ccfe7f916ccca008e
crispin <shoy1415@gmail.com>
2025-04-24 19:35:32 +0900

    :recycle: Refactor: rename class

--- lab-article/src/main/kotlin/com/crispinlab/article/config/ArticleGenerateIdConfig.kt
+++ lab-article/src/main/kotlin/com/crispinlab/article/config/ArticleGenerateIdConfig.kt
@@ -1,11 +1,13 @@
 package com.crispinlab.article.config
 
 import com.crispinlab.Snowflake
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class GenerateIdConfig {
+class ArticleGenerateIdConfig {
     @Bean
+    @ConditionalOnMissingBean(Snowflake::class)
     fun snowflake(): Snowflake = Snowflake.create()
 }
--- lab-board/src/main/kotlin/com/crispinlab/board/config/BoardGenerateIdConfig.kt
+++ lab-board/src/main/kotlin/com/crispinlab/board/config/BoardGenerateIdConfig.kt
@@ -1,11 +1,13 @@
 package com.crispinlab.board.config
 
 import com.crispinlab.Snowflake
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class GenerateIdConfig {
+class BoardGenerateIdConfig {
     @Bean
+    @ConditionalOnMissingBean(Snowflake::class)
     fun snowflake(): Snowflake = Snowflake.create()
 }
