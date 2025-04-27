bd798c8e7ceb2702f95b064eea1e4c33bfe72d24
crispin <h.c.shin.dev09@gmail.com>
2025-04-27 20:11:10 +0900

    :sparkles: Feat: add board artcle domain

---
+++ lab-board/src/main/kotlin/com/crispinlab/board/application/domain/model/BoardArticle.kt
@@ -0,0 +1,14 @@
+package com.crispinlab.board.application.domain.model
+
+import com.crispinlab.article.application.domain.model.VisibilityType
+import java.time.Instant
+
+data class BoardArticle(
+    val articleId: Long,
+    val boardId: Long,
+    val title: String,
+    val content: String,
+    val author: Long,
+    val visibility: VisibilityType,
+    val modifiedAt: Instant
+)
