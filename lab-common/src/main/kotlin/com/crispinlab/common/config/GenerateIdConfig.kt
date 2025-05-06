b433c33408aab3ed98ed400408bbac07813c2b13
crispin <shoy1415@gmail.com>
2025-05-06 17:02:55 +0900

    :wrench: Config: add common module

--- lab-common/src/main/kotlin/com/crispinlab/common/config/GenerateIdConfig.kt
+++ lab-common/src/main/kotlin/com/crispinlab/common/config/GenerateIdConfig.kt
@@ -1,13 +1,11 @@
-package com.crispinlab.board.config
+package com.crispinlab.common.config
 
 import com.crispinlab.Snowflake
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class BoardGenerateIdConfig {
+class GenerateIdConfig {
     @Bean
-    @ConditionalOnMissingBean(Snowflake::class)
     fun snowflake(): Snowflake = Snowflake.create()
 }
