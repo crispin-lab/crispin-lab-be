d520466837c946a95c02324fadcd4b816b02191d
crispin <h.c.shin.dev09@gmail.com>
2025-05-06 22:16:46 +0900

    :white_check_mark: Test: add update board controller test

--- lab-board/src/test/kotlin/com/crispinlab/board/adapter/input/web/ManageBoardWebControllerV1Test.kt
+++ lab-board/src/test/kotlin/com/crispinlab/board/adapter/input/web/ManageBoardWebControllerV1Test.kt
@@ -1,8 +1,10 @@
 package com.crispinlab.board.adapter.input.web
 
 import com.crispinlab.board.adapter.input.web.dto.request.CreateBoardRequest
+import com.crispinlab.board.adapter.input.web.dto.request.UpdateBoardRequest
 import com.crispinlab.board.fake.FakeManageBoardUseCase
 import com.crispinlab.common.config.KotlinSerializerConfig
+import java.time.Instant
 import kotlinx.serialization.json.Json
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
@@ -60,6 +62,49 @@ class ManageBoardWebControllerV1Test {
                 // then
                 result
                     .andExpect(MockMvcResultMatchers.status().isOk)
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("Update")
+    inner class UpdateBoardControllerTest {
+        @Nested
+        @DisplayName("Success")
+        inner class UpdateBoardSuccessTest {
+            @Test
+            @DisplayName("게시판 업데이트 요청 성공 테스트")
+            fun updateBoardRequestTest() {
+                // given
+                val request =
+                    UpdateBoardRequest(
+                        30972317390639104,
+                        "테스트 수정 게시판",
+                        "테스트 수정 게시판 입니다.",
+                        UpdateBoardRequest.VisibilityType.PRIVATE,
+                        Instant.now()
+                    )
+
+                // when
+                val result: ResultActions =
+                    mockMvc
+                        .perform(
+                            MockMvcRequestBuilders
+                                .patch("/api/board")
+                                .content(
+                                    json.encodeToString(
+                                        UpdateBoardRequest.serializer(),
+                                        request
+                                    )
+                                ).accept("application/vnd.crispin-lab.com-v1+json")
+                                .contentType(MediaType.APPLICATION_JSON)
+                        ).andDo(MockMvcResultHandlers.print())
+
+                // then
+                result.andExpectAll(
+                    MockMvcResultMatchers.status().isOk,
+                    MockMvcResultMatchers.jsonPath("$.resultCode").value("SUCCESS")
+                )
             }
         }
     }
