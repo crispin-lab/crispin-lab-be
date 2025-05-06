89760f644452aa34f1e4a71d93df096838a5df30
crispin <shoy1415@gmail.com>
2025-05-06 17:06:09 +0900

    :white_check_mark: Test: add board manage controller test

---
+++ lab-board/src/test/kotlin/com/crispinlab/board/adapter/input/web/ManageBoardWebControllerV1Test.kt
@@ -0,0 +1,66 @@
+package com.crispinlab.board.adapter.input.web
+
+import com.crispinlab.board.adapter.input.web.dto.request.CreateBoardRequest
+import com.crispinlab.board.fake.FakeManageBoardUseCase
+import com.crispinlab.common.config.KotlinSerializerConfig
+import kotlinx.serialization.json.Json
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.context.annotation.Import
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.ResultActions
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+
+@Import(FakeManageBoardUseCase::class, KotlinSerializerConfig::class)
+@WebMvcTest(ManageBoardWebControllerV1::class)
+class ManageBoardWebControllerV1Test {
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Autowired
+    private lateinit var json: Json
+
+    @Nested
+    @DisplayName("Create")
+    inner class ManageBoardControllerTest {
+        @Nested
+        @DisplayName("Success")
+        inner class CreateBoardSuccessTest {
+            @Test
+            @DisplayName("게시판 생성 요청 성공 테스트")
+            fun createBoardRequestTest() {
+                // given
+                val request =
+                    CreateBoardRequest(
+                        name = "테스트 게시판",
+                        visibilityType = CreateBoardRequest.VisibilityType.PUBLIC
+                    )
+
+                // when
+                val result: ResultActions =
+                    mockMvc
+                        .perform(
+                            MockMvcRequestBuilders
+                                .post("/api/board")
+                                .content(
+                                    json.encodeToString(
+                                        CreateBoardRequest.serializer(),
+                                        request
+                                    )
+                                ).accept("application/vnd.crispin-lab.com-v1+json")
+                                .contentType(MediaType.APPLICATION_JSON)
+                        ).andDo(MockMvcResultHandlers.print())
+
+                // then
+                result
+                    .andExpect(MockMvcResultMatchers.status().isOk)
+            }
+        }
+    }
+}
---
+++ lab-board/src/test/kotlin/com/crispinlab/board/fake/FakeManageBoardUseCase.kt
@@ -0,0 +1,27 @@
+package com.crispinlab.board.fake
+
+import com.crispinlab.board.application.port.input.ManageBoardUseCase
+import com.crispinlab.board.fixture.snowflake
+import java.time.Instant
+
+class FakeManageBoardUseCase : ManageBoardUseCase {
+    override fun create(
+        request: ManageBoardUseCase.CreateRequest
+    ): ManageBoardUseCase.CreateResponse =
+        ManageBoardUseCase.CreateResponse(
+            id = snowflake.nextId(),
+            createdAt = Instant.now()
+        )
+
+    override fun update(
+        request: ManageBoardUseCase.UpdateRequest
+    ): ManageBoardUseCase.UpdateResponse {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(
+        request: ManageBoardUseCase.DeleteRequest
+    ): ManageBoardUseCase.DeleteResponse {
+        TODO("Not yet implemented")
+    }
+}
